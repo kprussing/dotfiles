efb4f93e9e9d3749677db68b04608226131298c2
kprussing <kprussing74@gmail.com>
2015-12-27 15:08:54 -0500

    Add completion options

--- zshrc.d/options.zsh
+++ zshrc.d/options.zsh
@@ -3,10 +3,12 @@
 
 # If the pattern is bad, shut up and  pass it to the command and let it
 # sort it all out.  This is especially true for using `port`.
+
 setopt no_nomatch
 setopt no_badpattern
 
 # Make history work nicely.
+
 setopt append_history           # Yes, save the history,
 setopt inc_append_history       # write it straight away,
 setopt share_history            # and all terminals can see it.
@@ -17,8 +19,17 @@ setopt hist_reduce_blanks       # Remove superfluous white space.
 setopt hist_ignore_space        # Ignore lines starting with a space.
 setopt hist_no_store            # Ignore me searching my history.
 setopt hist_no_functions        # Proper functions go in a file.
-setopt hist_verify              # Verify the history expansion before 
+setopt hist_verify              # Verify the history expansion before
                                 # execution.
+# Completions
+
+setopt auto_list            # Show a list on ambiguous completion.
+setopt no_menu_complete     # Don't automatically insert a completion.
+setopt no_auto_remove_slash # Leave the slash.  I'm used to it.
+setopt complete_in_word     # Yes, complete a word I'm in the middle of.
+setopt always_to_end        # And just go to the end of the word once 
+                            # I'm done.
+
 # Miscellaneous
 
 setopt extended_glob            # Use better REGEX for globbing
