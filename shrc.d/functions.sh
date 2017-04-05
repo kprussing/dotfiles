3f2c000759e3d86bb7f85d10007208c74722bdc8
kprussing <kprussing74@gmail.com>
2017-04-05 12:06:24 -0400

    Add path prepend function
    This is really helpful when prepending LD_LIBRARY_PATH during
    development.

--- shrc.d/functions.sh
+++ shrc.d/functions.sh
@@ -17,6 +17,23 @@ function path_append () {
     return
 }
 
+function path_prepend () {
+    # Prepend a path ($2) to a variable ($1) if it is not already in the
+    # variable and exists.
+    if [[ -z "$1" ]]; then
+        # If the string is uninitialized, just create the path
+        [ -d $2 ] && echo $2
+    else
+        # Check for duplicate paths.
+        if echo $1 | grep -q "$2"; then
+            echo $1
+        else
+            [ -d $2 ] && echo $2:$1 || echo $1
+        fi
+    fi
+    return
+}
+
 function path_remove () {
     # Remove a path ($2) from a variable ($1) if it occurs in the path.
     # Don't forget to clean up those ugly duplicate colons.
